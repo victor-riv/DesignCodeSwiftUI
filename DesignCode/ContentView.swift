580a6f20715da6a5e737a27c4c01e96e38d3f789
Victor Rivera <verm2405@icloud.com>
2020-05-20 21:18:29 -0400

    saving up to gestures

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,131 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Victor Rivera on 5/19/20.
+//  Copyright © 2020 Victor Rivera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var show = false
+    var body: some View {
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+            BackCardView()
+                .background(show ? Color("card3") : Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: show ? 0 : 10))
+                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+            BackCardView()
+                .background(show ? Color("card4") : Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: show ? 0 : 5))
+                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
+            CardView()
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+            }
+            BottomCardView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 220)
+        
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("This certificate is proof that Victor Rivera has achieved the UI Design course with approval from a Design+Code instructor")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 500)
+    }
+}
